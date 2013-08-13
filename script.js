331218585a65e44c2dc767a25e96dabfeb08e62a
Yan Hao Leon Shen <yhls.xyz791@gmail.com>
2013-08-13 13:13:03 +1000

    clicking on a post title will now scroll smoothly to it

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function scrollToPosition(position)
+{
+	var from = {count: $(document).scrollTop()};
+	var to = {count: position};
+	
+	$(from).animate(to, {duration: 100, step: function (stepNumber, fx) {
+		$(document).scrollTop(stepNumber);
+	}});
+}
+
+function scrollToPost(postid)
+{
+	var target = $(postid).parent().offset().top - 60;
+	scrollToPosition(target);
+}
+
+$(document).ready(function () {
+	$(".post a").bind("click", function (e) {
+		var target = $(e.currentTarget).attr("href");
+		scrollToPost(target);
+	});
+});
