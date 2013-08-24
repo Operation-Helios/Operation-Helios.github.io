8c77e5df3dc7ae51664d456b7e34a3828577949e
Yan Hao Leon Shen <yhls.xyz791@gmail.com>
2013-08-24 16:11:49 +1000

    updated experimental comment loading code

--- script.js
+++ script.js
@@ -27,16 +27,18 @@ function openBox(element) {
 		var title = $(element).parent().children('.title').html();
 		var id = $(opened).parent().children('.anchor').attr('id');
 		var url = $(location).attr('origin') + $(location).attr('pathname') + '#' + id;
-		alert(title);
 		
-		var code = '<div id=\'disqus_thread\'></div><script type=\'text/javascript\'>var disqus_shortname=\''
-		+shortname+'\';var disqus_identifier=\''+id+'\';var disqus_title=\''+title+'\';var disqus_url=\''
-		+url+'\';(function(){var dsq = document.createElement(\'script\'); dsq.type = \'text/javascript\';'
-		+'dsq.async = true;dsq.src = \'//\' + disqus_shortname + \'.disqus.com/embed.js\';'
-		+'(document.getElementsByTagName(\'head\')[0] || document.getElementsByTagName(\'body\')[0]).appendChild(dsq);})();'
-		+'</script><noscript>Please enable JavaScript to view the <a href=\'http://disqus.com/?ref_noscript\'>'
-		+'comments powered by Disqus.</a></noscript><a href=\'http://disqus.com\' class=\'dsq-brlink\'>comments powered by '
-		+'<span class=\'logo-disqus\'>Disqus</span></a>';
+		var code = '<div id="disqus_thread"></div>'
+    	+'<script type="text/javascript">'
+        +'var disqus_shortname = \'operationhelios\';'
+        +'(function() {'
+        +'	var dsq = document.createElement(\'script\'); dsq.type = \'text/javascript\'; dsq.async = true;'
+        +'	dsq.src = \'//\' + disqus_shortname + \'.disqus.com/embed.js\';'
+        +'	(document.getElementsByTagName(\'head\')[0] || document.getElementsByTagName(\'body\')[0]).appendChild(dsq);'
+        +'})();'
+    	+'</script>'
+    	+'<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>'
+    	+'<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>';
 		newBox.children('iframe').contents().find('.container').html(code);
 		
 		var h = box.children('iframe').contents().find('html').height() + 20;
