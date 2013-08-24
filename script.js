0ab2730ffeef2b77b25b2262ecb9407686640be2
Yan Hao Leon Shen <yhls.xyz791@gmail.com>
2013-08-24 16:43:15 +1000

    disabled commenting code

--- script.js
+++ script.js
@@ -14,54 +14,53 @@ function scrollToPost(postid) {
 	}});
 }
 
-function openBox(element) {
-	$(element).parent().append('<div class=\'comment-box\'></div>');
-	newBox = $(element).next('.comment-box');
-	newBox.html('<iframe src=\'comments.html\'></iframe>');
-	newBox.css('height', 0);
+// function openBox(element) {
+// 	$(element).parent().append('<div class=\'comment-box\'></div>');
+// 	newBox = $(element).next('.comment-box');
+// 	newBox.html('<iframe src=\'comments.html\'></iframe>');
+// 	newBox.css('height', 0);
 	
-	newBox.children('iframe').on('load', function (e) {
-		var box = $(e.currentTarget).parent();
+// 	newBox.children('iframe').on('load', function (e) {
+// 		var box = $(e.currentTarget).parent();
 		
-		var shortname = 'operationhelios';
-		var title = $(element).parent().children('.title').html();
-		var id = $(opened).parent().children('.anchor').attr('id');
-		var url = $(location).attr('origin') + $(location).attr('pathname') + '#' + id;
+// 		var shortname = 'operationhelios';
+// 		var title = $(element).parent().children('.title').html();
+// 		var id = $(opened).parent().children('.anchor').attr('id');
+// 		var url = $(location).attr('origin') + $(location).attr('pathname') + '#' + id;
 		
-		var code = '<div id="disqus_thread"></div>'
-    	+'<script type="text/javascript">'
-        +'var disqus_shortname = \'operationhelios\';'
-        +'(function() {'
-        +'	var dsq = document.createElement(\'script\'); dsq.type = \'text/javascript\'; dsq.async = true;'
-        +'	dsq.src = \'//\' + disqus_shortname + \'.disqus.com/embed.js\';'
-        +'	(document.getElementsByTagName(\'head\')[0] || document.getElementsByTagName(\'body\')[0]).appendChild(dsq);'
-        +'})();'
-    	+'</script>'
-    	+'<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>'
-    	+'<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>';
-		newBox.children('iframe').contents().find('.container').html(code);
+// 		var code = '\
+// 		<script type="text/javascript">\
+// 			(function() {\
+// 	            var dsq = document.createElement(\'script\'); dsq.type = \'text/javascript\'; dsq.async = true;\
+// 	            dsq.src = \'http://operationhelios.disqus.com/embed.js\';\
+// 	            (document.getElementsByTagName(\'head\')[0] || document.getElementsByTagName(\'body\')[0]).appendChild(dsq);\
+// 	        })();\
+// 		</script>';
 		
-		var h = box.children('iframe').contents().find('html').height() + 20;
+//     	newBox.children('iframe').contents().find('.container').append('<div id="disqus_thread"></div>');
+// 		newBox.children('iframe').contents().find('.container').append(code);
 		
-		if (!isMobile) {
-			box.css('height', 0);
-			box.animate({height: h}, {duration: 100});
-		}
-		else
-			box.height(h);
-	});
-}
+// 		var h = box.children('iframe').contents().find('html').height() + 20;
+		
+// 		if (!isMobile) {
+// 			box.css('height', 0);
+// 			box.animate({height: h}, {duration: 100});
+// 		}
+// 		else
+// 			box.height(h);
+// 	});
+// }
 
-function closeBox(element) {
-	oldBox = $(element).next('.comment-box');
-	if (!isMobile) {
-		oldBox.animate({height: 0}, {duration: 100, done: function () {
-			oldBox.remove();
-		}});
-	}
-	else
-		oldBox.remove();
-}
+// function closeBox(element) {
+// 	oldBox = $(element).next('.comment-box');
+// 	if (!isMobile) {
+// 		oldBox.animate({height: 0}, {duration: 100, done: function () {
+// 			oldBox.remove();
+// 		}});
+// 	}
+// 	else
+// 		oldBox.remove();
+// }
 
 $(document).ready(function () {
 	if (navigator.userAgent.match(/Mobile|Android|iPhone|iPod|iPad/gi)) {
@@ -76,24 +75,24 @@ $(document).ready(function () {
 		});
 	}
 	
-	$('.comments-button').html('Load comments');
-	$('.comments-button').bind('click', function (e) {
-		if (opened == 0) {
-			opened = e.currentTarget;
-			openBox(opened);
-			$(opened).html('Hide comments');
-		}
-		else if (opened == e.currentTarget) {
-			closeBox(opened);
-			$(opened).html('Load comments');
-			opened = 0;
-		}
-		else {
-			closeBox(opened);
-			$(opened).html('Load comments');
-			opened = e.currentTarget;
-			openBox(opened);
-			$(opened).html('Hide comments');
-		}
-	});
+	// $('.comments-button').html('Load comments');
+	// $('.comments-button').bind('click', function (e) {
+	// 	if (opened == 0) {
+	// 		opened = e.currentTarget;
+	// 		openBox(opened);
+	// 		$(opened).html('Hide comments');
+	// 	}
+	// 	else if (opened == e.currentTarget) {
+	// 		closeBox(opened);
+	// 		$(opened).html('Load comments');
+	// 		opened = 0;
+	// 	}
+	// 	else {
+	// 		closeBox(opened);
+	// 		$(opened).html('Load comments');
+	// 		opened = e.currentTarget;
+	// 		openBox(opened);
+	// 		$(opened).html('Hide comments');
+	// 	}
+	// });
 });
